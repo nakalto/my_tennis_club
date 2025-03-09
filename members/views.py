0f8a81e1aaadfacd672459b5780669b0241d100e
anacrethus_alto <altoanacrethus@gmail.com>
2025-03-09 14:49:56 +0300

    template and view where updsted , myfirst. html deled

--- members/views.py
+++ members/views.py
@@ -4,10 +4,6 @@ from django.template import loader
 from . models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
-
-def members(request):
     mymembers = Member.objects.all().values()
     template = loader.get_template("all_members.html")
 
