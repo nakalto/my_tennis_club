48d6f97fdacffe09820011a9c9238c1b645fa701
anacrethus_alto <altoanacrethus@gmail.com>
2025-03-10 17:31:54 +0300

    admin page created and displayed

--- members/admin.py
+++ members/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from . models import Member
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname","lastname","joined_date")
+
+    
+admin.site.register(Member,MemberAdmin)
+
+
